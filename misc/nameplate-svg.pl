69c491d1444d5d099003fee1d30284bb303b7786
miyagawa <miyagawa@65690182-5df0-0310-9871-95da5a3706e7>
2008-04-27 16:03:24 +0000

    pseudonymous

--- misc/nameplate-svg.pl
+++ misc/nameplate-svg.pl
@@ -22,8 +22,8 @@ $csv->column_names(@$header);
 
 my $out_dir = "$ENV{HOME}/Sites/yapc-nameplates";
 
-my $font = '&quot;M+ 1c&quot;';
-#my $font = 'Helvetica';
+#my $font = '&quot;M+ 1c&quot;';
+my $font = 'mplus-1c-medium';
 
 my @users;
 while (!$csv->eof) {
@@ -45,20 +45,20 @@ while (my @chunk = splice(@users, 0, 10)) {
         my $size = length($name) > 20 ? 18 
                  : length($name) > 16 ? 21 
                  : 24;
-        $svg->text(x => $ox + 35, y => $oy + 55, style => { 'font-family' => $font, 'font-weight' => 'bold', 'color' => 'black', 'font-size' => $size })->cdata($name);
-        $svg->text(x => $ox + 35, y => $oy + 75, style => { 'font-family' => $font, 'color' => 'black', 'font-size' => 11 })->cdata(($ref->{pm_group} ? "$ref->{pm_group} / " : "") . code2country($ref->{country}));
+        $svg->text(x => $ox + 35, y => $oy + 55, style => { 'font-family' => $font, 'font-weight' => 'bold', 'color' => 'black', 'font-size' => $size }, 'font-family' => $font, 'font-size' => $size)->cdata($name);
+        $svg->text(x => $ox + 35, y => $oy + 75, style => { 'font-family' => $font, 'color' => 'black', 'font-size' => 11 }, 'font-family' => $font, 'font-size' => 11)->cdata(($ref->{pm_group} ? "$ref->{pm_group} / " : "") . code2country($ref->{country}));
         if ($ref->{company}) {
-            $svg->text(x => $ox + 35, y => $oy + 90, style => { 'font-family' => $font, color => 'black', 'font-size' => 11 })->cdata( decode_utf8($ref->{company}) );
+            $svg->text(x => $ox + 35, y => $oy + 90, style => { 'font-family' => $font, color => 'black', 'font-size' => 11 }, 'font-family' => $font, 'font-size' => 11)->cdata( decode_utf8($ref->{company}) );
         }
 
-        my $role = $ref->{has_talk} ? 'SPEAKER' :
-            ($ref->{is_orga} || $ref->{is_staff}) ? 'STAFF' : '';
+        my $role = ($ref->{is_orga} || $ref->{is_staff}) ? 'STAFF' : 
+            $ref->{has_talk} ? 'SPEAKER' :'';
 
         if ($role) {
-            $svg->text(x => $ox + 35, y => $oy + 126, style => { 'font-family' => $font, 'font-weight' => 'bold', color => 'black', 'font-size' => 16 })->cdata($role);
+            $svg->text(x => $ox + 35, y => $oy + 126, style => { 'font-family' => $font, 'font-weight' => 'bold', color => 'black', 'font-size' => 16 }, 'font-family' => $font, 'font-size' => 16)->cdata($role);
         }
 
-        $svg->text(x => $ox + 266, y => $oy + 16, style => { 'font-family' => $font, color => 'black', 'font-size' => 8 })->cdata($ref->{user_id});
+        $svg->text(x => $ox + 266, y => $oy + 16, style => { 'font-family' => $font, color => 'black', 'font-size' => 8 }, 'font-family' => $font, 'font-size' => 8)->cdata($ref->{user_id});
 
         $ox = $ox == 0 ? 296 : 0;
         $oy+= 168 if $ox == 0;
@@ -73,7 +73,10 @@ while (my @chunk = splice(@users, 0, 10)) {
 sub get_name {
     my $ref = shift;
 
-    ## xxx nickname
+    if ($ref->{pseudonymous}) {
+        return decode_utf8( $ref->{nick_name} );
+    }
+
     if (is_cjk($ref)) {
         return decode_utf8( $ref->{last_name} . $ref->{first_name} );
     } else {
